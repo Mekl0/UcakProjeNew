89d236c36a95a62f68c0c83c4701c2aee6b9be13
Mekl0 <130905519+Mekl0@users.noreply.github.com>
2024-01-21 22:29:28 +0300

    Add files via upload

---
+++ Ucakprojesi-main/Musteri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcakProje
+{
+    public  class Musteri
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int yas {  get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public string Kalkistarihi { get; set; }
+    }
+}
---
+++ Ucakprojesi-main/Rezervasyon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcakProje
+{
+    internal class Rezervasyon
+    {
+        public Musteri Musteri  { get; set; }
+        public Ucak Ucak { get; set; }
+        
+        public Lokasyon Lokasyon { get; set; }
+        public Ucus Ucus { get; set; }
+        
+        
+        
+    }
+}
